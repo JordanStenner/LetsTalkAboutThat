904f817d6ac985075d3cd7f0dff75838f5010de0
Jordan Stenner <jordan.stenner@students.plymouth.ac.uk>
2020-11-26 10:42:57 +0000

    Finished start server code

---
+++ startserver.js
@@ -0,0 +1,21 @@
+let express = require("express");
+let path = require("path");
+
+let app = express();
+let port = 9000;
+
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "static")));
+app.set("view engine", "ejs");
+
+app.get("", function(request, response){
+    response.render("landing");
+});
+
+app.get("/createaccount", function(request, response){
+    response.render("createaccount");
+});
+
+app.listen(port, function(){
+    console.log("Listening on " + port);
+});
